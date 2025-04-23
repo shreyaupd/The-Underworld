4b0ce2e777a26a1b716c4b0190af1a6a166e8a3d
shreyaupd <shreyaupd234@gmail.com>
2025-04-23 20:38:50 +0545

    chnages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
     return (
         <div className="sticky left-0 top-0 z-50 w-full h-20 bg-transparent flex items-center px-4">
             <div className=" w-full">
-                <ul className="flex justify-center items-center flex-row gap-50">
+                <ul className="flex justify-center items-center flex-row gap-25">
                     <li>Home</li>
                     <li>About</li>
                     <li>LOGO</li>
