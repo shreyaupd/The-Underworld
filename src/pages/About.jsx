71931ae35f7be1401b15b286d71aaf5d29542961
shreyaupd <shreyaupd234@gmail.com>
2025-05-03 09:50:32 +0545

    add About component to Home page

---
+++ src/pages/About.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div className="container mx-auto px-6 py-8">
+        <div className="innerbox">
+          <div className="about">
+            <h1 className="text-3xl font-bold mb-4">About</h1>
+          </div>
+
+          <div className="heading mb-4 -mt-8">
+            International Dive Training Agency
+          </div>
+
+          <div className="contents">
+            The best diving experience with our professional,friendly and
+            dedicated team. We offer daily diving and snorkeling trip to
+            glorious reefs around the globe.
+          </div>
+
+          <div className="photos mt-4">
+            <img src="" alt="photos" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Navbar from '../components/Navbar'
+import About from './About'
 const Home = () => {
   return (
     <>
     <Navbar/>
      <Hero/>
+     <About/>
     </>
   )
 }
